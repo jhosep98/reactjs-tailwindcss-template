f6c8c8cad9ed9a619547d7b9fa80ac81aee463bb
jhosep98 <jhosep149@hotmail.com>
2020-12-03 20:17:09 -0300

    finished project setup and junk file cleanup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      <img
+        alt="random_image"
+        className="w-full"
+        src="https://source.unsplash.com/random"
+      />
+      <div className="px-6 py-4">
+        <div className="font-bold text-purple-500 text-xl mb-2 ">
+          Photo by John Doe
+        </div>
+      </div>
     </div>
   );
 }
