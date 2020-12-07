db34086728365f812b6d5a6dcb6ed296dd3c5af3
jhosep98 <jhosep149@hotmail.com>
2020-12-07 19:40:09 -0300

    customize button

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
+import { Button } from "./components/Button";
+
 function App() {
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <img
-        alt="random_image"
-        className="w-full"
-        src="https://source.unsplash.com/random"
-      />
-      <div className="px-6 py-4">
-        <div className="font-bold text-purple-500 text-xl mb-2 ">
-          Photo by John Doe
-        </div>
-      </div>
+    <div className="container mx-auto">
+      <Button />
     </div>
   );
 }
