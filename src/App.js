26d0b821583b04cc3f5fe8bf5a8350f2b35ca8b8
jhosep98 <jhosep149@hotmail.com>
2020-12-11 23:02:25 -0300

    template reactjs and tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import { Button } from "./components/Button";
-
 function App() {
   return (
-    <div className="container mx-auto">
-      <Button />
+    <div className="container mx-auto flex flex-col justify-center items-center my-5">
+      <h1 className="text-lg font-serif font-bold">Hello Tailwindcss!</h1>
+      <button className="bg-blue-500 sm:bg-red-400 md:bg-purple-600 hover:bg-blue-700 text-white font-bold py-2 px-4 my-4 rounded">
+        My Button
+      </button>
     </div>
   );
 }
--- src/components/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Button = () => {
-  return (
-    <button className="bg-blue-500 sm:bg-red-400 md:bg-purple-600 hover:bg-blue-700 text-white font-bold py-2 px-4 my-4 rounded">
-      My Button
-    </button>
-  );
-};
